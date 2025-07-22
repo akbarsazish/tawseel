a319cd1cbfdd23c33f7b536bd1fa5ae9e31da40c
akbarsazish <sazishaliakbar@gmail.com>
2025-07-22 20:58:30 +0330

    add contract to repository

---
+++ app/Views/contract/footer.php
@@ -0,0 +1,36 @@
+<table class="info-table">
+    <tr>
+        <td colspan="3">Mina Al Fahal</td>
+        <td>P.O.Box</td>
+        <td>1513</td>
+        <td class="rtl">صندوق بريد</td>
+        <td>C.R</td>
+        <td>1456949</td>
+        <td class="rtl">رقم السجل التجاري</td>
+    </tr>
+    <tr>
+        <td colspan="3" class="rtl">ميناء الفحل</td>
+        <td>Postal Code</td>
+        <td>116</td>
+        <td class="rtl">رمز البريد</td>
+        <td>OCCI</td>
+        <td>1514861</td>
+        <td class="rtl">رقم الغرفة التجارية</td>
+    </tr>
+    <tr>
+        <td colspan="3" class="bold">Address</td>
+        <td colspan="5" class="rtl bold">شارع  17،  بناية الصامد <span style="font-family: Arial;">, 17 Street, Al Samed Building</span></td>
+        <td  class="rtl bold"> العنوان </td>
+    </tr>
+    <tr>
+        <td>Flat</td>
+        <td>7820K</td>
+        <td class="rtl">شقة</td>
+        <td>WhatsApp</td>
+        <td>92657648</td>
+        <td class="rtl">رقم الواتساب</td>
+        <td>Phone</td>
+        <td>99634433</td>
+        <td class="rtl">رقم الهاتف</td>
+    </tr>
+</table>
---
+++ app/Views/contract/header.php
@@ -0,0 +1,18 @@
+<table class="info-table" style="width: 100%;">
+    <tbody>
+        <tr>
+            <td style="width: 15%;">
+                <img src="<?= $logo ?>" alt="Tawseel Logo" style="height: 80px;">
+            </td>
+            <td colspan="5" align="center" style="width: 70%;">
+                <p class="headerItem"><?= $companyName_en ?></p>
+                <p class="headerItem"><?= $companyName_ar ?></p>
+                <p class="headerItem"><?= $companyName1_en ?></p>
+                <p class="headerItem"><?= $companyName2_ar ?></p>
+            </td>
+            <td style="width: 15%;">
+                <img src="<?= $logo ?>" alt="Tawseel Logo" style="height: 80px;">
+            </td>
+        </tr>
+    </tbody>
+</table>
