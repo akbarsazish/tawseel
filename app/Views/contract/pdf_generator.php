99937071f1b29d544e065a7dd8d8458716ae419f
akbarsazish <sazishaliakbar@gmail.com>
2025-07-26 07:58:15 +0330

    footer style has been changed

--- app/Views/contract/pdf_generator.php
+++ app/Views/contract/pdf_generator.php
@@ -44,7 +44,7 @@
         vertical-align: middle;
     }
 
-    .info-table .rtl {
+    .info-table, .footerTable .rtl {
         direction: rtl;
         text-align: right;
         font-family: Tahoma, 'Arial', sans-serif;
